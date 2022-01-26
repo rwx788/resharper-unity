3b5533c425f3ed3717819b47ab66fa478e0a59ef
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2022-01-26 23:24:52 +0300

    fix zoning problems

--- resharper/resharper-unity/test/src/Unity.Rider.Tests/UnityRider/CSharp/Daemon/Stages/Analysis/UnityEventFunctionAnalyzerTests.cs
+++ resharper/resharper-unity/test/src/Unity.Rider.Tests/UnityRider/CSharp/Daemon/Stages/Analysis/UnityEventFunctionAnalyzerTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using JetBrains.Application.Settings;
-using JetBrains.Diagnostics;
+﻿using JetBrains.Application.Settings;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.FeaturesTestFramework.Daemon;
 using JetBrains.ReSharper.Plugins.Tests.Unity;
@@ -10,7 +6,7 @@ using JetBrains.ReSharper.Plugins.Unity.CSharp.Daemon.Stages.Highlightings;
 using JetBrains.ReSharper.Psi;
 using NUnit.Framework;
 
-namespace JetBrains.ReSharper.Plugins.Tests.UnityRider.CSharp.Daemon.Stages.Analysis
+namespace JetBrains.ReSharper.Plugins.Unity.Rider.Tests.UnityRider.CSharp.Daemon.Stages.Analysis
 {
     [TestUnity]
     public class UnityEventFunctionAnalyzerTests : CSharpHighlightingTestBase
@@ -28,14 +24,5 @@ namespace JetBrains.ReSharper.Plugins.Tests.UnityRider.CSharp.Daemon.Stages.Anal
         // ********************************************************************
 
         [Test] public void TestUnityEventFunctionAnalyzer() { DoNamedTest2(); }
-
-        protected override IList<string> TraceCategories()
-        {
-            myLogger.Info($"MyConfigPath1: {Environment.GetEnvironmentVariable("RESHARPER_LOG_CONF")}");
-            myLogger.Info($"MyConfigPath2: {Environment.GetEnvironmentVariable("RESHARPER_HOST_LOG_DIR")}");
-            var result = base.TraceCategories().ToList();
-            result.Add("JetBrains.Application.Environment"); 
-            return result;
-        }
     }
 }
